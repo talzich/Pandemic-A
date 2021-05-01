b45849063ba71781f77b2f55a9d12f3e287b709c
Tal <talzhtc@gmail.com>
2021-05-01 15:19:52 +0300

    Added method declarations

--- Player.hpp
+++ Player.hpp
@@ -2,3 +2,29 @@
 // AUTHOR: Tal Zichlinsky
 // Header file for class player
 
+namespace pandemic{
+    class Player{
+
+        public:
+            Player(const Board &board, const City &city);
+            ~Player();
+
+            void drive(const City &city);
+
+            void fly_direct(const City &city);
+
+            void fly_charter(const City &city);
+
+            void fly_shuttle(const City &city);
+
+            void build();
+
+            void discover_cure(const Color &color);
+
+            void treat(const City &city);
+
+            void role();
+
+            void take_card(const City &city);
+    };
+}
