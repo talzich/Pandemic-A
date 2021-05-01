e5b8755900b8fc8bda1f6b9574f7cd862c083a26
Tal <talzhtc@gmail.com>
2021-05-01 18:33:04 +0300

    Vacously implemented required methods

---
+++ FieldDoctor.cpp
@@ -0,0 +1,11 @@
+//
+// AUTHOR: Tal Zichlinsky
+// This file is used to build the FieldDoctor class as described in this assignment's README.md file
+
+#include "FieldDoctor.hpp"
+
+namespace pandemic{
+
+    void FieldDoctor::treat(const City &city){}
+    
+}
--- FieldDoctor.hpp
+++ FieldDoctor.hpp
@@ -3,6 +3,7 @@
 // Header file for class FieldDoctor
 
 #include "Player.hpp"
+#include "City.hpp"
 
 namespace pandemic{
     class FieldDoctor: public Player{
