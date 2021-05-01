ef3e9d88541bbc6addfc77fcd6bbd631bf1405cb
Tal <talzhtc@gmail.com>
2021-05-01 19:06:05 +0300

    Vacously implemented required methods

---
+++ Virologist.cpp
@@ -0,0 +1,10 @@
+//
+// AUTHOR: Tal Zichlinsky
+// This file is used to build the Virologist class as described in this assignment's README.md file
+
+#include "Virologist.hpp"
+
+namespace pandemic{
+
+    Player &Virologist::treat(const City &City){ return *this; }
+}
--- Virologist.hpp
+++ Virologist.hpp
@@ -2,12 +2,15 @@
 // AUTHOR: Tal Zichlinsky
 // Header file for class Virologist
 
+#include "City.hpp"
+#include "Player.hpp"
+
 namespace pandemic{
     class Virologist: public Player{
 
         using Player::Player;
         
         // Overloading method from Player
-        void treat(const City &city);
+        Player &treat(const City &city);
     };
 }
